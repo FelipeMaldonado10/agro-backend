3b56d84fa6cccb806d1233fd27562f4e2eb18555
FelipeMaldonado10 <afmaldonado10@gmail.com>
2025-08-16 21:22:40 -0500

    agregar nombre a la informacion del token

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const generateToken = (user) => {
   return jwt.sign(
-    { id: user._id, rol: user.rol, email: user.email },
+    { id: user._id, rol: user.rol, email: user.email, nombre: user.nombre },
     process.env.JWT_SECRET,
     { expiresIn: '7d' }
   );
