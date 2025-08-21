2c08fade3b2d2957584c9bb648e96d8b700d3c30
Ivan Daniel Palacio Hernandez <ipalacio@ucundinamarca.edu.co>
2025-08-20 20:11:18 -0500

    paquetes
    paquetes

--- src/app.js
+++ src/app.js
@@ -18,6 +18,9 @@ app.use('/api/users', require('./routes/user.routes'));
 <<<<<<< Updated upstream
 =======
 app.use('/api/parcelas', require('./routes/parcela.routes'));
+app.use('/api/recomendaciones', require('./routes/recommendation.routes'));
+app.use('/api/market-prices', require('./routes/marketPrice.routes'));
+app.use('/api/productos', require('./routes/producto.routes'));
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
