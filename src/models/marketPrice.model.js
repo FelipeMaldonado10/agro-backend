c9e073e219a65699636ba2322f7bcfd3d58989f8
NyawToo <julianandresperez904@hotmail.com>
2025-08-24 10:40:39 -0500

    marketprice models y routes

--- src/models/marketPrice.model.js
+++ src/models/marketPrice.model.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const MarketPriceSchema = new mongoose.Schema({
-  producto: { type: String, required: true },
-  ciudad: { type: String, required: true },
+  producto: { type: mongoose.Schema.Types.ObjectId, ref: 'Producto', required: true },
+  ciudad: { type: mongoose.Schema.Types.ObjectId, ref: 'Ciudad', required: true },
   fecha: { type: String, required: true }, // YYYY-MM-DD
   precio: { type: Number, required: true }
 });
